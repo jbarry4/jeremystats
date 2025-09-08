50167f7fe786affa7b2c65e257494805c4adcc5a
Shahriar Akhavan Tafti <theexaminedexistence@gmail.com>
2025-09-08 15:26:21 -0400

    Create main.m

---
+++ IED/main.m
@@ -0,0 +1,14 @@
+LLSpikeViewer("C:\Users\Barry Lab\Desktop\TestIEDData\M13s2aug1\2023-08-01_12-11-26\LL_input_M13s2aug1_2023-08-01_12-11-26_mex_disk.mat", "C:\Users\Barry Lab\Desktop\TestIEDData\M13s2aug1\2023-08-01_12-11-26\LL_input_M13s2aug1_2023-08-01_12-11-26_mex_disk_LLspikes_20250908_120233.mat")
+
+
+Invalid or deleted object.
+
+Error in LLSpikeViewer/refreshPlot (line 158)
+        onlySel = logical(chkOnlySel.Value);
+
+Error in LLSpikeViewer (line 104)
+refreshPlot();
+
+Error in main (line 1)
+LLSpikeViewer("C:\Users\Barry Lab\Desktop\TestIEDData\M13s2aug1\2023-08-01_12-11-26\LL_input_M13s2aug1_2023-08-01_12-11-26_mex_disk.mat", "C:\Users\Barry Lab\Desktop\TestIEDData\M13s2aug1\2023-08-01_12-11-26\LL_input_M13s2aug1_2023-08-01_12-11-26_mex_disk_LLspikes_20250908_120233.mat")
+ 
