1420a51285ff0e8633eff8df71e937296c7b47e4
maliajaff <theexaminedexistence@gmail.com>
2025-09-15 15:49:08 -0400

    Update main.m

--- IED/main.m
+++ IED/main.m
@@ -6,3 +6,6 @@
 TheVisionOverlay("C:\Users\Z390\Desktop\IED DATA\LL_input_M13s2aug1_2023-08-01_12-11-26_mex_disk.mat","C:\Users\Z390\Desktop\IED DATA\LL_input_M13s2aug1_2023-08-01_12-11-26_mex_disk_LLspikes_20250909_134703.mat")
 %TheVisionOverlayByPolarity("C:\Users\Z390\Desktop\IED DATA\LL_input_M13s2aug1_2023-08-01_12-11-26_mex_disk.mat","C:\Users\Z390\Desktop\IED DATA\LL_input_M13s2aug1_2023-08-01_12-11-26_mex_disk_LLspikes_20250909_134703.mat")
 
+%%%next step... sort into Solid, Sputter,Garbage, Flag... and then...
+%%%create channel wide average wave form (so e.g average of many solids, to
+%%%give you 32 average waveforms to visualize Xplorefinder style
