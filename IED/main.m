60130e3d362503d09ae0b530fc12be8e025ae518
maliajaff <theexaminedexistence@gmail.com>
2025-09-23 16:17:05 -0400

    Update main scripts to use Pipeline_Main function
    Commented out direct calls to CSD_CenterSlices_Waveforms_AvgGroups and added Pipeline_Main to streamline the main report pipeline. Added output and warning messages in main.m for panel composition failures, improving debugging and workflow clarity.

--- IED/main.m
+++ IED/main.m
@@ -20,4 +20,15 @@
 
 Pipeline_Main("C:\Users\Z390\Desktop\IED DATA\Preped Data - M13s2aug1","C:\Users\Z390\Desktop\IED DATA\LL_input_M13s2aug1_2023-08-01_12-11-26_mex_disk_uV.mat")
 
- 
+ %%Fix master view. Pipeline donw
+
+ Composing 3-column panel → C:\Users\Z390\Desktop\IED DATA\Preped Data - M13s2aug1\Pipeline Output\Master_Compact_SOLID.png
+Warning: Failed to build SOLID compact: Unable to perform assignment because the size of the left
+side is 2-by-2524-by-3 and the size of the right side is 1-by-1-by-3. 
+> In Pipeline_Main (line 97)
+In main (line 21) 
+Composing 3-column panel → C:\Users\Z390\Desktop\IED DATA\Preped Data - M13s2aug1\Pipeline Output\Master_Compact_SPUTTER.png
+Warning: Failed to build SPUTTER compact: Unable to perform assignment because the size of the left
+side is 2-by-2513-by-3 and the size of the right side is 1-by-1-by-3. 
+> In Pipeline_Main (line 109)
+In main (line 21) 
